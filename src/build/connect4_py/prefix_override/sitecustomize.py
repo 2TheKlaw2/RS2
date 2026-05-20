6e99af2339227a2cbffa03b702486ffd4b925fc7
coach-zain <zzk2301@gmail.com>
2026-05-20 21:42:24 +1000

    Remove erroneous build, install, log files

--- src/build/connect4_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/enriques/rs2/src/install/connect4_py'
--- src/build/connect4_py/share/connect4_py/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/enriques/rs2/src/build/connect4_py"
